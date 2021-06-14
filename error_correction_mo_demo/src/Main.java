483291bb56a3ee2921f4fd956f0d489fbc80ac00
mortezakhoramdeldreadful <50551637+mortezakhoramdeldreadful@users.noreply.github.com>
2021-06-14 15:38:28 +0430

    init

--- error_correction_mo_demo/src/Main.java
+++ error_correction_mo_demo/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("mortezza");
+       NetworkHandler.getInstance().init();
+       byte[] bytes = NetworkHandler.getInstance().receiveFrame();
+        System.out.println(bytes);
+
     }
 }
