8640927e780d591b004e1dc6fa3da44974daf279
javidchaji <46443872+javidchaji@users.noreply.github.com>
2021-06-15 19:14:41 +0430

    Checksum Demodulator

--- error_correction_mo_demo/src/Checksum.java
+++ error_correction_mo_demo/src/Checksum.java
@@ -5,18 +5,18 @@ public class Checksum {
         // and for Header IP checksums only checksum is calculated from
         // the header for zero checksum.
         long tempSum = 0;
-        short finalSum = 0;
+        short temp = 0;
+        short currectVal = 0;
         short finvers = 0;
-        int temp = 0;
-
         long extera = 0;
 
         for(int i = 0 ; i < strb.length; i += 2){
-            temp = (int)strb[i] << 8 ;
+            temp = (short) (strb[i] << 8) ;
             if(i+1 < strb.length) {
-                temp = temp | strb[i + 1];
+                currectVal = (short) (strb[i + 1] & 0xff);
+                temp = (short) (temp | currectVal);
             }else {
-                temp = temp | 0x0000;
+                temp = (short) (temp | 0x0000);
             }
             tempSum += temp;
             extera = tempSum & 0x0000;
@@ -25,9 +25,42 @@ public class Checksum {
                 tempSum += extera;
             }
         }
-        finalSum = (short) tempSum;
-        finvers = (short) (finalSum ^ 0xffff);
+        finvers = (short) (tempSum ^ 0xffff);
         return finvers;
 
+    }
+
+
+    public static boolean checkChecksum(byte[] strb)
+    {
+        // Note: For UDP checksums all checksum is zero depending on itself
+        // and for Header IP checksums only checksum is calculated from
+        // the header for zero checksum.
+        long tempSum = 0;
+        short temp = 0;
+        short currectVal = 0;
+        long extera = 0;
+
+        for(int i = 0 ; i < strb.length; i += 2){
+            temp = (short) (strb[i] << 8) ;
+            if(i+1 < strb.length) {
+                currectVal = (short) (strb[i + 1] & 0xff);
+                temp = (short) (temp | currectVal);
+
+            }else {
+                temp = (short) (temp | 0x0000);
+            }
+            tempSum += temp;
+            extera = tempSum & 0x0000;
+            if(extera != 0){
+                extera = extera >> 16;
+                tempSum += extera;
+            }
+        }
+        if(tempSum == -1){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
