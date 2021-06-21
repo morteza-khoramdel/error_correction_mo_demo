603a996ea842a6b85e89100d739cbffcc509bc32
javidchaji <46443872+javidchaji@users.noreply.github.com>
2021-06-21 15:52:43 +0430

    DeModulation Functions

--- error_correction_mo_demo/src/DeModulation.java
+++ error_correction_mo_demo/src/DeModulation.java
@@ -4,6 +4,7 @@ import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Udp;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class DeModulation extends Thread {
 
@@ -36,6 +37,63 @@ public class DeModulation extends Thread {
                     destIP = org.jnetpcap.packet.format.FormatUtils.ip(dIP);
                     System.out.println(packet);
                     //TODO DEMODULATION
+
+                    byte[] newPayload = hammingCode.demodulatorDriver(udp.getPayload());
+
+                    ByteBuffer byteBuffers = ByteBuffer.allocate(packet.size() - (newPayload.length - udp.getPayload().length));
+                    packet.setByteBuffer(packet.size() - (newPayload.length - udp.getPayload().length) ,byteBuffers);
+                    packet.setSize(packet.size() - (newPayload.length - udp.getPayload().length));
+                    //udp payload
+                    packet.setByteArray(42, newPayload);
+                    //udp payload
+
+
+                    //ip total
+                    short tempIpTotal = 0;
+                    tempIpTotal = (short) (ip.length() - (newPayload.length - udp.getPayload().length));
+                    byte[] ipTotal;
+                    ipTotal = ByteBuffer.allocate(2).putShort(tempIpTotal).array();
+                    packet.setByteArray(16, ipTotal);
+                    //ip total
+
+                    //ip header check sum
+                    byte[] oldChecksumIP = packet.getByteArray(14, 33);
+                    oldChecksumIP[24 - 14] = 0;
+                    oldChecksumIP[25 - 14] = 0;
+                    boolean newCheckSumIP = Checksum.checkChecksum(oldChecksumIP);
+                    if(!newCheckSumIP) {
+                        //drop packet
+                        //finish
+                    }
+
+                    //ip header check sum
+
+
+                    //udp total
+                    short tempUDPTotal;
+                    tempUDPTotal = (short) (ip.length() - (newPayload.length - udp.getPayload().length));
+
+                    byte[] udpTotal;
+                    udpTotal = ByteBuffer.allocate(2).putShort(tempUDPTotal).array();
+                    packet.setByteArray(38, udpTotal);
+                    //udp total
+
+                    //udp header check sum
+                    System.out.println("totlal size is : " + packet.size());
+                    byte[] oldChecksumUdp;
+                    oldChecksumUdp = packet.getByteArray(34, packet.size() );
+                    oldChecksumUdp[40 - 34] = 0;
+                    oldChecksumUdp[41 - 34] = 0;
+                    boolean newCheckSumUdp = Checksum.checkChecksum(oldChecksumUdp);
+                    if(!newCheckSumUdp){
+                        //drop packet
+                        //finish
+                    }
+
+                    //udp header check sum
+                    System.out.println(packet);
+
+
                 }
 
 
